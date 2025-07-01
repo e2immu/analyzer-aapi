70c69a85cdfd44b30ebe9e4b49564df4b8f56664
Bart Naudts <Bart.Naudts@codelaser.io>
2025-07-01 20:13:27 +0200

    added a 24.0.1 jar

--- copyToJars.sh
+++ copyToJars.sh
@@ -1,4 +1,6 @@
 cd e2immu-aapi-archive/src/main/resources/org/e2immu/analyzer/aapi/archive/analyzedPackageFiles/jdk/openjdk-21.0.7
+jar cf ../openjdk-24.0.1.jar *.json
+cd ../openjdk-23.0.2/
 jar cf ../openjdk-21.0.7.jar *.json
 cd ../openjdk-23.0.2/
 jar cf ../openjdk-23.0.2.jar *.json
